c309d1be3abfae99429ecf54d3f9f64f2534c09a
VarunMayanger <mrmayanger@gmail.com>
2023-07-26 16:21:05 +0530

    changed port to 80

--- infra/main.tf
+++ infra/main.tf
@@ -87,6 +87,6 @@ resource "google_compute_global_forwarding_rule" "default" {
   load_balancing_scheme = "EXTERNAL"
   ip_address            = google_compute_global_address.website.address
   ip_protocol           = "TCP"
-  port_range            = "443"
+  port_range            = "80"
   target                = google_compute_target_https_proxy.website.self_link
 }
